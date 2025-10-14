abae48ddaf42d526801d630352ed4bd5fbb8faa0
Joe <zcai51@ucsc.edu>
2025-10-14 14:20:20 -0700

    Initial Commit

---
+++ blaze-lite/core/lib/spi_flash/spiFlash.h
@@ -0,0 +1,25 @@
+/*
+    spiFlash.h - a library that handles the Adafruit SPI flash breakout board for Blackpill Arduino
+    To use:
+        include this header file in your main program, DO NOT directly call the cpp file
+*/
+
+#ifndef SPI_FLASH_H
+#define SPI_FLASH_H
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <SdFat.h>
+
+#include <Adafruit_SPIFlash.h>
+
+class spiFlash {
+    public:
+
+    private:
+
+}
+
+
+
+#endif
