0c93b7008ab1d4bfd70217b5f26cfbf97b035710
nbaweja125 <nbaweja@ucsc.edu>
2025-11-10 15:25:43 -0800

    Added read and write function

--- blaze-lite/core/src/main.cpp
+++ blaze-lite/core/src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
-#include <iostream>
-#include <cstring>
+#include <SPI.h>
+#include <SD.h>
 
 const long baudrate = 115200;
+const int chipSelect = 3;
 
 void setup()
 {
@@ -10,7 +11,32 @@ void setup()
     while (!Serial) {
         delay(10);
     }
-    Serial.println("This is on the BlackPill board");
+    Serial.println("Initializing SD Card...");
+
+    if (!SD.begin()) {
+        Serial.println("Card failed, or not present");
+        return;
+    }
+    Serial.println("Card initialized.");
+    File myFile = SD.open("example.txt", FILE_WRITE);
+    if (myFile) {
+        myFile.println("Hello, world!");
+        myFile.close();
+        Serial.println("Wrote to file.");
+    } else {
+        Serial.println("Error opening file.");
+    }
+
+    myFile = SD.open("example.txt");
+    if (myFile) {
+        Serial.println("example.txt:");
+        while (myFile.available()) {
+            Serial.write(myFile.read());
+        }
+        myFile.close();
+    } else {
+        Serial.println("Error opening file.");
+    }
 }
 
 
