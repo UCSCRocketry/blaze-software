4cc0305d5df9d079793e7281e1d3ed97cbcb0873
nbaweja125 <nbaweja@ucsc.edu>
2025-12-01 15:28:15 -0800

    comments

--- blaze-lite/core/src/main.cpp
+++ blaze-lite/core/src/main.cpp
@@ -11,6 +11,30 @@ void setup()
         delay(10);
     }
     Serial.println("This is on the BlackPill board");
+     if (!SD.begin()) {
+        Serial.println("Card failed, or not present");
+        return;
+    }
+    Serial.println("Card initialized.");
+    File myFile = SD.open("example.txt", FILE_WRITE);
+    if (myFile) {
+        myFile.println("Hello, world!");
+        myFile.close();
+        Serial.println("Wrote to file.");
+    } else {
+        Serial.println("Error opening file.");
+    }
+
+    myFile = SD.open("example.txt");
+    if (myFile) {
+        Serial.println("example.txt:");
+        while (myFile.available()) {
+            Serial.write(myFile.read());
+        }
+        myFile.close();
+    } else {
+        Serial.println("Error opening file.");
+    }
 }
 
 
