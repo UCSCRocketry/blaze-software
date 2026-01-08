93eb669960fab179ac8298c9f769b1d2816f0a0f
Joe <zcai51@ucsc.edu>
2026-01-08 15:35:44 -0800

    added some basic functions to scCard.cpp

--- blaze-lite/core/lib/sdCard/sdCard.cpp
+++ blaze-lite/core/lib/sdCard/sdCard.cpp
@@ -6,3 +6,37 @@
 
 #include "sdCard.h"
 
+File sdFile; // global file object
+
+sdCard::SDCard(const int csPin, const size_t buffersize) {
+    this->CS_PIN = csPin;
+}
+
+sdCard::~SDCard() {
+    //might not need this
+}
+
+void sdCard::startUp() {
+    //sd card init
+    if (!SD.begin(this->CS_PIN)) {
+        Serial.println("Card failed, or not present");
+        return;
+    }
+    Serial.println("SD Card initialized.");
+    //open file
+    sdFile = SD.open("Log.txt", FILE_WRITE);
+    if (!sdFile) {
+        Serial.println("Error opening Log.txt");
+    }
+    //test read&write
+}
+
+char sdCard::getCS_PIN() {
+    return this->CS_PIN;
+}
+
+void sdCard::setCS_PIN(char pin) {
+    this->CS_PIN = pin;
+}
+
+
