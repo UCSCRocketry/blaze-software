9f2e98a0edbe379ac5e687178a4cecd190544eca
Joe <zcai51@ucsc.edu>
2026-01-15 14:23:24 -0800

    Revert "Switching Libary to LIBdriver"
    This reverts commit 8e95bc4b8cda8c1ef175f8f6a65f91cf661cd141.

--- blaze-lite/core/lib/Barometer/Baro.h
+++ blaze-lite/core/lib/Barometer/Baro.h
@@ -1,27 +1,43 @@
-#pragma once
 
 #include <Arduino.h>
-
-#include "driver_ms5611_interface.h"
+#include <SPI.h>
+#include <MS5611_SPI.h>
 
 class Baro {
     public:
-        //constructors/destructors
+        //constructor
         Baro(uint8_t csPin);
+        //destructor
         ~Baro();
 
         bool startUp();
 
-        bool read();
+        //read functions
+        int read();
+        uint32_t getDeviceID();
+        uint16_t getProm(uint8_t index);
+        uint16_t getCRC();
 
-        float getTemperature();     // Celsius
-        float getPressureMbar();    // mbar
-        float getPressurePa();      // Pascal
-        float getAltitudeM(float seaLevelPressureMbar = 1013.25f);
+        //configuration functions
+        void setOverSamplingRate(uint8_t osr);
+
+        void setPressureOffset(float offset);
+
+        //get methods
+        float getSeaLevelPressure();
+
+        //set methods
+        void setSeaLevelPressure(float pressure);
+
+        //sensor reading functions
+        float getTemperature();
+        float getPressure();
+        float getPressurePascal();
+        float getBaroAltitude(float seaLevelPressure);
+
 
     private:
-        ms5611_handle_t handle;
-        uint8_t csPin;
-        float temperatureC;
-        float pressureMbar;
+        MS5611_SPI baroObject;
+        uint8_t CS_PIN;
+        float seaLevelPressure; // in mbar
 };
