9be7d35885f77579d9f7c4003cc9a6855a33fd14
Joe <zcai51@ucsc.edu>
2026-03-30 15:25:35 -0700

    library finished, pending HW test

---
+++ blaze-lite/core/lib/IMU/LSM9DS1IMU.cpp
@@ -0,0 +1,125 @@
+/**
+ * @file LSM9DS1IMU.cpp
+ * @brief Implementation of LSM9DS1IMU class
+ */
+
+#include <Arduino.h>
+
+#include "LSM9DS1IMU.h"
+
+LSM9DS1IMU::LSM9DS1IMU(SPIClass &spiPort, uint8_t accelGyroCsPin, uint8_t magCsPin)
+    : _lsm9ds1(accelGyroCsPin, magCsPin, &spiPort),
+      _hasSample(false),
+      _isReady(false) {}
+
+LSM9DS1IMU::~LSM9DS1IMU() {}
+
+void LSM9DS1IMU::setUp() {
+    Serial.println("Initializing LSM9DS1 IMU...");
+    while (!_lsm9ds1.begin()) {
+        Serial.println("Failed to initialize LSM9DS1 IMU! Retrying...");
+        delay(1000);
+    }
+    _isReady = true;
+    Serial.println("Setting sensor range");
+    _lsm9ds1.setupAccel(_lsm9ds1.LSM9DS1_ACCELRANGE_16G, _lsm9ds1.LSM9DS1_ACCELDATARATE_952HZ);
+    _lsm9ds1.setupMag(_lsm9ds1.LSM9DS1_MAGGAIN_16GAUSS);
+    _lsm9ds1.setupGyro(_lsm9ds1.LSM9DS1_GYROSCALE_245DPS);
+}
+
+void LSM9DS1IMU::pollSensors() {
+    if (!_isReady) {
+        _hasSample = false;
+        return;
+    }
+
+    _lsm9ds1.read();
+    _lsm9ds1.getEvent(&_accel, &_mag, &_gyro, &_temp);
+    _hasSample = true;
+}
+
+bool LSM9DS1IMU::readAccel(float &x, float &y, float &z) {
+    if (!_isReady) {
+        x = 0.0f;
+        y = 0.0f;
+        z = 0.0f;
+        return false;
+    }
+    if (!_hasSample) {
+        pollSensors();
+    }
+    if (!_hasSample) {
+        x = 0.0f;
+        y = 0.0f;
+        z = 0.0f;
+        return false;
+    }
+
+    x = _accel.acceleration.x;
+    y = _accel.acceleration.y;
+    z = _accel.acceleration.z;
+    return true;
+}
+
+bool LSM9DS1IMU::readGyro(float &x, float &y, float &z) {
+    if (!_isReady) {
+        x = 0.0f;
+        y = 0.0f;
+        z = 0.0f;
+        return false;
+    }
+    if (!_hasSample) {
+        pollSensors();
+    }
+    if (!_hasSample) {
+        x = 0.0f;
+        y = 0.0f;
+        z = 0.0f;
+        return false;
+    }
+
+    x = _gyro.gyro.x;
+    y = _gyro.gyro.y;
+    z = _gyro.gyro.z;
+    return true;
+}
+
+bool LSM9DS1IMU::readMag(float &x, float &y, float &z) {
+    if (!_isReady) {
+        x = 0.0f;
+        y = 0.0f;
+        z = 0.0f;
+        return false;
+    }
+    if (!_hasSample) {
+        pollSensors();
+    }
+    if (!_hasSample) {
+        x = 0.0f;
+        y = 0.0f;
+        z = 0.0f;
+        return false;
+    }
+
+    x = _mag.magnetic.x;
+    y = _mag.magnetic.y;
+    z = _mag.magnetic.z;
+    return true;
+}
+
+bool LSM9DS1IMU::readTemp(float &celsius) {
+    if (!_isReady) {
+        celsius = 0.0f;
+        return false;
+    }
+    if (!_hasSample) {
+        pollSensors();
+    }
+    if (!_hasSample) {
+        celsius = 0.0f;
+        return false;
+    }
+
+    celsius = _temp.temperature;
+    return true;
+}
---
+++ blaze-lite/core/test/LSM9DS1IMUTest.cpp
@@ -0,0 +1,77 @@
+#include <Arduino.h>
+#include <SPI.h>
+
+#include "LSM9DS1IMU.h"
+
+#define LSM9DS1_XGCS_PIN PB12
+#define LSM9DS1_MCS_PIN PB13
+
+LSM9DS1IMU imu(SPI, LSM9DS1_XGCS_PIN, LSM9DS1_MCS_PIN);
+
+void setup() {
+    Serial.begin(9600);
+    while (!Serial) {
+        delay(100);
+    }
+
+    SPI.begin();
+
+    pinMode(LSM9DS1_XGCS_PIN, OUTPUT);
+    pinMode(LSM9DS1_MCS_PIN, OUTPUT);
+    digitalWrite(LSM9DS1_XGCS_PIN, HIGH);
+    digitalWrite(LSM9DS1_MCS_PIN, HIGH);
+
+    imu.setUp();
+}
+
+void loop() {
+    float ax, ay, az;
+    float gx, gy, gz;
+    float mx, my, mz;
+    float tempC;
+
+    imu.pollSensors();
+
+    if (imu.readAccel(ax, ay, az)) {
+        Serial.print("ACC (m/s^2): ");
+        Serial.print(ax, 4);
+        Serial.print(", ");
+        Serial.print(ay, 4);
+        Serial.print(", ");
+        Serial.println(az, 4);
+    } else {
+        Serial.println("ACC: read failed");
+    }
+
+    if (imu.readGyro(gx, gy, gz)) {
+        Serial.print("GYRO (dps): ");
+        Serial.print(gx, 4);
+        Serial.print(", ");
+        Serial.print(gy, 4);
+        Serial.print(", ");
+        Serial.println(gz, 4);
+    } else {
+        Serial.println("GYRO: read failed");
+    }
+
+    if (imu.readMag(mx, my, mz)) {
+        Serial.print("MAG (gauss): ");
+        Serial.print(mx, 4);
+        Serial.print(", ");
+        Serial.print(my, 4);
+        Serial.print(", ");
+        Serial.println(mz, 4);
+    } else {
+        Serial.println("MAG: read failed");
+    }
+
+    if (imu.readTemp(tempC)) {
+        Serial.print("TEMP (C): ");
+        Serial.println(tempC, 2);
+    } else {
+        Serial.println("TEMP: read failed");
+    }
+
+    Serial.println("---");
+    delay(200);
+}
